55a4d401e0deabb3feb5725259c65f74e5d63638
Jordan Fox <jordan.fox@canstar.com.au>
2023-06-25 19:43:33 +1000

    changed script

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
 import './App.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCTNrQIJe0Q2-OsvqeskacQ6bYJfcDCvR0",
+  authDomain: "todo-list-82592.firebaseapp.com",
+  projectId: "todo-list-82592",
+  storageBucket: "todo-list-82592.appspot.com",
+  messagingSenderId: "597007571250",
+  appId: "1:597007571250:web:129d32bf17d9e3ccff1f2b",
+  measurementId: "G-LT6LHE7WTS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
 
   return (

a8878d820a00a063cad30839524b78cc78e99da7
Jordan Fox <jordan.fox@canstar.com.au>
2023-06-25 20:05:45 +1000

    fixing deploy script

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import './App.css'
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// const firebaseConfig = {
 
-};
+// };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 function App() {
 

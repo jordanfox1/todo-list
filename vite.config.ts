ebafc7dea33d3bce8d80ebb3639882981202be0a
Jordan Fox <jordan.fox@canstar.com.au>
2023-07-16 10:45:37 +1000

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": process.env,
-    VITE_NODE_ENV: process.env.VITE_NODE_ENV,
-  },
 });
